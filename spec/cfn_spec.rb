5a7333ba4208ddf9f7a2298c9c4f7469db169151
Sean Edwards <stedwards87+git@gmail.com>
2015-05-31 14:19:46 -0400

    Added a test for following stack events

--- spec/cfn_spec.rb
+++ spec/cfn_spec.rb
@@ -28,4 +28,49 @@ describe Cfer::Cfn::CfnStack do
 
     Cfer::Cfn::CfnStack.new('test', cfn).converge stack
   end
+
+
+  it 'follows logs' do
+    cfn = double("cfn")
+    yielder = double('yield receiver')
+
+    event_list = [
+      double(event_id: 1234, timestamp: DateTime.now, resource_status: 'TEST', resource_type: 'Cfer::TestResource', logical_resource_id: 'test_resource', resource_status_reason: 'abcd'),
+      double(event_id: 5678, timestamp: DateTime.now, resource_status: 'TEST2', resource_type: 'Cfer::TestResource', logical_resource_id: 'test_resource', resource_status_reason: 'efgh'),
+    ]
+
+    event_list_2 = [
+      double(event_id: 1234, timestamp: DateTime.now, resource_status: 'TEST', resource_type: 'Cfer::TestResource', logical_resource_id: 'test_resource', resource_status_reason: 'abcd'),
+      double(event_id: 5678, timestamp: DateTime.now, resource_status: 'TEST2', resource_type: 'Cfer::TestResource', logical_resource_id: 'test_resource', resource_status_reason: 'efgh'),
+    ]
+
+    event_list_3 = [
+      double(event_id: 1234, timestamp: DateTime.now, resource_status: 'TEST', resource_type: 'Cfer::TestResource', logical_resource_id: 'test_resource', resource_status_reason: 'abcd'),
+      double(event_id: 5678, timestamp: DateTime.now, resource_status: 'TEST_COMPLETE', resource_type: 'Cfer::TestResource', logical_resource_id: 'test_resource', resource_status_reason: 'efgh'),
+    ]
+
+    expect(cfn).to receive(:describe_stack_events)
+      .exactly(3).times
+      .with(stack_name: 'test')
+      .and_return(
+        [ double(stack_events: event_list) ],
+        [ double(stack_events: event_list_2) ],
+        [ double(stack_events: event_list_3) ]
+      )
+
+    expect(cfn).to receive(:describe_stacks)
+      .exactly(2).times
+      .with(stack_name: 'test')
+      .and_return(
+        double(stacks: [ double(stack_status: 'a status') ]),
+        double(stacks: [ double(stack_status: 'TEST_COMPLETE')])
+      )
+
+    expect(yielder).to receive(:yielded).with(event_list.first)
+
+    Cfer::Cfn::CfnStack.new('test', cfn).tail(number: 1, follow: true) do |event|
+      yielder.yielded event
+    end
+
+  end
 end
